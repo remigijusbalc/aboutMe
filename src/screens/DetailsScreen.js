f9212f4f12e137415063944ef9642ce2c83e4112
milremapp <milremapp@gmail.com>
2019-03-05 18:23:27 +0200

    skills screens added with information about skill,s experience and education

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -9,26 +9,55 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { skills } from "../helpers";
+import { ProgressBar, GradientView } from "../components";
+import { connect } from "react-redux";
 type Props = {};
-export default class DetailsScreen extends Component<Props> {
+
+import { Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+//Guideline sizes are based on standard ~5" screen mobile device
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+const scale = size => width / guidelineBaseWidth * size;
+const verticalScale = size => height / guidelineBaseHeight * size;
+const moderateScale = (size, factor = 0.5) => size + (scale(size) - size) * factor;
+
+class DetailsScreen extends Component<Props> {
   render() {
+    const { translations } = this.props;
+    console.log(translations,"trans");
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>DETAILS</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <GradientView style={{flex: 1}}>
+        <Text style={styles.welcome}>Skills</Text>
+        {Object.keys(skills).map(value => {
+          return(
+            <View style={{flex: 0, flexDirection: "row" , justifyContent: "space-evenly"}}>
+            <Icon name="label" size={20} color="black"/>
+            <Text style={{ fontSize: moderateScale(20)}}>{value}</Text>
+            <ProgressBar percent={skills[value]} width={150}/>
+            </View>
+          )
+        })}
+        <Text style={styles.welcome}>Experience</Text>
+        <Text style={styles.welcome}>Education</Text>
+        </GradientView>
+
     );
   }
 }
+
+const mapDispatchToProps = state => {
+  return {
+    translations: state.language.translations
+  }
+}
+
+export default connect(mapDispatchToProps, null)(DetailsScreen);
 
 const styles = StyleSheet.create({
   container: {
