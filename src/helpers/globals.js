c6ace1e32dbd9b94194360366aaa3e66048d7bb1
milremapp <milremapp@gmail.com>
2019-03-05 18:26:38 +0200

    added globals file to easily modify mapped info

---
+++ src/helpers/globals.js
@@ -0,0 +1,20 @@
+export const languageImages = {
+    lt: "ltflag",
+    en: "usflag"
+};
+
+export const skills = {
+    "JavaScript": 80,
+    "React": 60,
+    "React-Native": 80,
+    "Swift": 40,
+    "Node.js": 60,
+    "C#": 40,
+    "Photoshop": 50,
+    "Java": 30
+};
+
+export default {
+    languageImages,
+    skills
+};
