74519047ffcb17ad724cc47910de8ecd3427ad2f
milremapp <milremapp@gmail.com>
2019-03-13 22:11:54 +0200

    added progress bar to show skill level

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { View } from "react-native";
 
-
 export default (ProgressBar = ({ percent, width }) => {
-    const barWidth = width * (percent / 100);
+  const barWidth = width * (percent / 100);
 
   return (
-    <View style={{ borderWidth: 1, borderColor: "lightgrey", width: width }}>
+    <View
+      style={{
+        borderWidth: 1,
+        borderColor: "lightgrey",
+        width: width,
+        height: 20
+      }}
+    >
       <View
         style={{
           width: barWidth,
-          height: 16,
-          backgroundColor: "green"
+          height: "100%",
+          backgroundColor: "#e0ebfc",
+          flexWrap: "wrap"
         }}
       />
     </View>
