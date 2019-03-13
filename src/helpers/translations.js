ad6f3e2c269b74aa1640153a9345019379296fe9
milremapp <milremapp@gmail.com>
2019-03-13 22:10:45 +0200

    added more language properties

--- src/helpers/translations.js
+++ src/helpers/translations.js
@@ -1,37 +1,88 @@
 import { getPassedTime } from "./helperFunctions";
+import { getWorkExperience } from "./helperFunctions";
 
-
-export default DICTIONARY = {
-    description: {
-        lt: "As esu blablabla",
-        en: "Hi, I am blablalba"
+export default (DICTIONARY = {
+  description: {
+    lt: "As esu blablabla",
+    en: "Hi, I am blablalba"
+  },
+  Home: {
+    lt: "Profilis",
+    en: "Profile"
+  },
+  homeProffesion: {
+    lt: "Mobilių aplikacijų kūrėjas",
+    en: "Mobile application developer"
+  },
+  homeButton: {
+    lt: "Plačiau",
+    en: "More"
+  },
+  homeDescription: {
+    lt: "As Remis esu is tadada",
+    en: "Im rmgl from lt"
+  },
+  workExperience: {
+    lt: getWorkExperience("lt"),
+    en: getWorkExperience("en")
+  },
+  personalEducation: {
+    lt: {
+      university: "Vilniaus Kolegijos Elektronikos ir Informatikos fakultetas",
+      degree: "Informatikos inžinerijos profesinis bakalauras",
+      field: "Išmaniųjų įrenginių technologijos"
     },
-    title: {
-        lt: "Profilis",
-        en: "Profile"
-    },
-    workExperienec: {
-        lt: {title: "Mobilių aplikacijų kurėjas su React-Native karkasu", duration: getPassedTime("lt")},
-        en: {title:"Mobile application developer with React-Native framework", duration: getPassedTime("en")}
-    },
-    personalEducation: {
-        lt: {university: "Vilniaus Kolegijos Elektronikos ir Informatikos fakultetas", degree: "Informatikos inžinerijos profesinis bakalauras", field: "Išmaniųjų įrenginių technologijos"},
-        en: {university: "Vilniaus Kolegija/University of Applied Sciences", degree: "Professional Bachelor of Informatics Engineering", field: "Smart Device Technology"}
-    },
-    skills: {
-        lt: "Įgudžiai",
-        en: "Skills"
-    },
-    education: {
-        lt: "Išsilavinimas",
-        en: "Education"
+    en: {
+      university: "Vilniaus Kolegija/University of Applied Sciences",
+      degree: "Professional Bachelor of Informatics Engineering",
+      field: "Smart Device Technology"
     }
-
-}
+  },
+  Details: {
+    lt: "Įgudžiai",
+    en: "Skills"
+  },
+  education: {
+    lt: "Išsilavinimas",
+    en: "Education"
+  },
+  experience: {
+    lt: "Patirtis",
+    en: "Experience"
+  },
+  Hobbies: {
+    lt: "Laisvalaikis",
+    en: "Hobbies"
+  },
+  drumming: {
+    lt: {
+      title: "Grojimas būgnais",
+      description: "Groju bugnais blablabla",
+      openVideos: "Peržiūrėti"
+    },
+    en: {
+      title: "Drumming",
+      description: "I play drums blablalba",
+      openVideos: "Check out"
+    }
+  },
+  football: {
+    lt: { title: "Futbolas", description: "Megstu zaisti futbola" },
+    en: { title: "Football", description: "I like to play football" }
+  },
+  jogging: {
+    lt: { title: "Bėgiojimas", description: "Man patinka begioti" },
+    en: { title: "Jogging", description: "I like jogging" }
+  },
+  books: {
+    lt: { title: "Skaitymas", description: "Megstu skaityti knygas" },
+    en: { title: "Reading books", description: "I like to read books" }
+  }
+});
 
 export const TRANSLATIONS = (lang = "lt") => {
-    return Object.keys(DICTIONARY).reduce((acc,val) => {
+  return Object.keys(DICTIONARY).reduce((acc, val) => {
     acc[val] = DICTIONARY[val][lang];
     return acc;
-},{});
-}
+  }, {});
+};
