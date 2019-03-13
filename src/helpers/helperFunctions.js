6d49fbbe149d5d6e8ddb3c4431ecf51ddab0dcfc
milremapp <milremapp@gmail.com>
2019-03-13 22:10:09 +0200

    helper methods for translations

--- src/helpers/helperFunctions.js
+++ src/helpers/helperFunctions.js
@@ -1,8 +1,15 @@
 import moment from "moment/min/moment-with-locales";
+import { workExperience } from "./globals";
 
-export const getPassedTime = (lang) => {
+export const getPassedTime = lang => {
   moment.locale(lang);
-    const date = ["2018", "05", "14"];
   const startDate = moment(["2018", "05", "14"]);
   return moment(startDate).fromNow(true);
+};
+
+export const getWorkExperience = lang => {
+  return Object.keys(workExperience).reduce((acc, value) => {
+    acc[value] = workExperience[value][lang];
+    return acc;
+  }, {});
 };
