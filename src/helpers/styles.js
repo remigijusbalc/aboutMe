44df620d4e11362c62ffa735af848b0b4739d9ed
milremapp <milremapp@gmail.com>
2019-03-13 22:09:24 +0200

    global styles

---
+++ src/helpers/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  h1: { fontSize: 40, textAlign: "center" }
+});
+
+export const TEXT = {
+  H1: styles.h1
+};
+
+export default {
+  TEXT
+};
