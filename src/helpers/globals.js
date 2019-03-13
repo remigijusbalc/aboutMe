2db312e32aa5a9ffa99cb7f75dee4705a14cdf57
milremapp <milremapp@gmail.com>
2019-03-13 22:08:49 +0200

    additional globals added for easier maintenance

--- src/helpers/globals.js
+++ src/helpers/globals.js
@@ -1,20 +1,39 @@
+import { getPassedTime } from "./helperFunctions";
+
 export const languageImages = {
-    lt: "ltflag",
-    en: "usflag"
+  lt: "ltflag",
+  en: "usflag"
 };
 
-export const skills = {
-    "JavaScript": 80,
-    "React": 60,
-    "React-Native": 80,
-    "Swift": 40,
-    "Node.js": 60,
-    "C#": 40,
-    "Photoshop": 50,
-    "Java": 30
+export const skills = [
+  { name: "JavaScript", percent: 80, expanded: false },
+  { name: "React", percent: 60, expanded: false },
+  { name: "React-Native", percent: 80, expanded: false },
+  { name: "Swift", percent: 40, expanded: false },
+  { name: "Node.js", percent: 60, expanded: false },
+  { name: "C#", percent: 40, expanded: false },
+  { name: "Photoshop", percent: 50, expanded: false },
+  { name: "Java", percent: 30, expanded: false }
+];
+
+export const remoteAccounts = [
+  {
+    imageName: "linkedin",
+    uri: "https://www.linkedin.com/in/remigijus-bal%C4%8Di%C5%ABnas-b47152163/"
+  },
+  { imageName: "github", uri: "https://github.com/remigijusbalc" }
+];
+
+export const workExperience = {
+  "Lexita UAB": {
+    lt: { title: "Aplikacijų kūrėjas", duration: getPassedTime("lt") },
+    en: { title: "Mobile developer", duration: getPassedTime("en") }
+  }
 };
 
 export default {
-    languageImages,
-    skills
+  languageImages,
+  skills,
+  workExperience,
+  remoteAccounts
 };
