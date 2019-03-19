1ca9e514ee79957b00ddaafcdc7185a6f88c61a9
Remigijus <remigijus.balc@gmail.com>
2019-03-19 20:16:30 +0200

    MyAvatar export

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as LanguageComponent } from "./LanguageComponent";
 export { default as GradientView } from "./GradientView";
 export { default as ProgressBar } from "./ProgressBar";
 export { default as Card } from "./Card";
+export { default as MyAvatar } from "./MyAvatar";
