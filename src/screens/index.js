e59756d5396916f6b3f5c399d7b4f08794d4476a
Remigijus <remigijus.balc@gmail.com>
2019-03-22 17:03:01 +0200

    exports

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,5 @@
-export {default as DetailsScreen} from "./DetailsScreen";
-export {default as HobbiesScreen} from "./HobbiesScreen";
-export {default as HomeScreen} from "./HomeScreen";
-export {default as InitialScreen} from "./InitialScreen";
+export { default as DetailsScreen } from "./DetailsScreen";
+export { default as HobbiesScreen } from "./HobbiesScreen";
+export { default as HomeScreen } from "./HomeScreen";
+export { default as InitialScreen } from "./InitialScreen";
+export { default as SwipeParent } from "./SwipeParent";
