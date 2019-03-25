b6732e15e57ff428b90c8524cfa34fe6b5e69553
milremapp <milremapp@gmail.com>
2019-03-25 23:10:38 +0200

    added function to change screens

--- src/screens/SwipeParent.js
+++ src/screens/SwipeParent.js
@@ -3,11 +3,35 @@ import Swiper from "react-native-swiper";
 import HomeScreen from "./HomeScreen";
 import DetailsScreen from "./DetailsScreen";
 import HobbiesScreen from "./HobbiesScreen";
-//style={styles.wrapper}
-export default (SwipeParent = () => (
-  <Swiper showsButtons>
-    <HomeScreen />
-    <HobbiesScreen />
-    <DetailsScreen />
-  </Swiper>
-));
+
+export default class SwipeParent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.availableScreens = ["Home", "Details", "Hobbies"];
+
+    this.state = {
+      index: 0
+    };
+  }
+
+  changePage = name => {
+    const index = this.availableScreens.indexOf(name);
+    this.swiperRef.scrollBy(index);
+    //-1 * (0 - index)
+  };
+  render() {
+    return (
+      <Swiper
+        ref={swiperRef => (this.swiperRef = swiperRef)}
+        showsPagination={false}
+        index={0}
+        showsButtons={false}
+      >
+        <HomeScreen changePage={this.changePage} />
+        <DetailsScreen />
+        <HobbiesScreen />
+      </Swiper>
+    );
+  }
+}
