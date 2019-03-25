a664d0054551fd202dfe324f8330fb86946130f6
milremapp <milremapp@gmail.com>
2019-03-25 23:08:01 +0200

    exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as ProgressBar } from "./ProgressBar";
 export { default as Card } from "./Card";
 export { default as MyAvatar } from "./MyAvatar";
 export { default as HobbyIcon } from "./HobbyIcon";
+export { default as Button } from "./Button";
