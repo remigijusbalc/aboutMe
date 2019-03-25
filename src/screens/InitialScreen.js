a6b0ae1558382e6e738ae82cf08ddb5b9d7f193c
milremapp <milremapp@gmail.com>
2019-03-25 23:12:48 +0200

    added language change component

--- src/screens/InitialScreen.js
+++ src/screens/InitialScreen.js
@@ -1,45 +1,59 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, ActivityIndicator } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
 import { connect } from "react-redux";
 import { setLanguage, getLanguage } from "../redux/actions";
-import { LanguageComponent } from "../components";
+import { LanguageComponent, GradientView } from "../components";
 
 class InitialScreen extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: true
-        }
-    }
+    this.state = {
+      loading: true
+    };
+  }
 
-     componentDidMount(){
-         this.props.getLanguage()
-        .then(() => this.props.navigation.navigate("App"))
-        .catch(() => this.setState({ loading: false}));
-    }
+  componentDidMount() {
+    this.props
+      .getLanguage()
+      .then(() => this.props.navigation.navigate("App"))
+      .catch(() => this.setState({ loading: false }));
+  }
 
-    handleLanguageSelect(lang){
-        return this.props.setLanguage(lang)
-        .then(() => this.props.navigation.navigate("App"));
-    }
+  handleLanguageSelect(lang) {
+    return this.props
+      .setLanguage(lang)
+      .then(() => this.props.navigation.navigate("App"));
+  }
 
-
-    render(){
-        const { loading } = this.state;
-        return(
-            <View style={{ alignItems: "center", justifyContent: "center", flex: 1}}>
-            {loading ?
-              <ActivityIndicator size="large" color="#0000ff" /> : 
-                <LanguageComponent onSelect={this.handleLanguageSelect.bind(this)}/>}
-            </View>
-        )
-    }
+  render() {
+    const { loading } = this.state;
+    return (
+      <GradientView
+        style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
+      >
+        {loading ? (
+          <ActivityIndicator size="large" color="#2897B0" />
+        ) : (
+          <LanguageComponent onSelect={this.handleLanguageSelect.bind(this)} />
+        )}
+      </GradientView>
+    );
+  }
 }
 
-export default connect((state) => {
-  return { language: state.language}
-},{
+export default connect(
+  state => {
+    return { language: state.language };
+  },
+  {
     setLanguage,
     getLanguage
-})(InitialScreen);
+  }
+)(InitialScreen);
